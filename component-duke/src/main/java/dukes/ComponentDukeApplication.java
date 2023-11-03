f4b3cafb8a945a50f3778fc6e029f02cf731da92
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2023-11-03 13:14:16 +0100

    Restructure

---
+++ component-duke/src/main/java/dukes/ComponentDukeApplication.java
@@ -0,0 +1,14 @@
+package eu.agilejava.dukes;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("")
+public class ComponentDukeApplication extends Application {
+
+    void main() throws InterruptedException {
+
+
+    }
+
+}
