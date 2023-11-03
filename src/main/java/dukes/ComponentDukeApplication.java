1e4f3c8c0e918eb7cd7b0fe39dc440801a374359
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2023-11-03 13:03:18 +0100

    Initial

---
+++ src/main/java/dukes/ComponentDukeApplication.java
@@ -0,0 +1,15 @@
+package eu.agilejava.dukes;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("")
+public class ComponentDukeApplication extends Application {
+
+    void main() throws InterruptedException {
+
+        
+
+    }
+
+}
